7234c2b3491d2d6cd5971bb13d9672fbe4115a47
Abderrahmane Mahmi <elmahmi.a04@gmail.com>
2020-06-19 16:21:23 +0000

    Added text files as tests

--- 1.5 Union-Find/quick-find.py
+++ 1.5 Union-Find/quick-find.py
@@ -6,15 +6,23 @@ class QuickFind:
     for i in range(totalElements):
       self.points.append(i)
 
+  def __repr__(self):
+    return "QuickFind{}".format(tuple(self.points))
+
   def union(self, p, q):
-    if self.points[p] == self.points[q]:
-      return
+    if self.points[p] == self.points[q]: return
+    for i, j in enumerate(self.points):
+      if j == self.points[q]:
+        self.points[i] = self.points[p]
 
-    for i in self.points:
-      if i == self.points[q]:
-        self.points = 1
-
-
-
+  def connected(self, p, q):
+    return self.points[p] == self.points[q]
 
 
+with open("tinyUF.txt", "r") as f:
+  data = f.read().split("\n")
+  links = [tuple(i.split(" ")) for i in data[1:-1]]
+  
+qf = QuickFind(int(data[0]))
+for p, q in links:
+  qf.union(int(p), int(q))
