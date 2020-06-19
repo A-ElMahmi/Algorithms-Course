f876ffa2752cfdc795bd0be93bd46aa40430fb10
Abderrahmane Mahmi <elmahmi.a04@gmail.com>
2020-06-19 19:31:30 +0000

    Added Weighted Quick Union and fixing the union method in Quick Union

--- 1.5 Union-Find/weighted-quick-union.py
+++ 1.5 Union-Find/weighted-quick-union.py
@@ -1,23 +1,16 @@
-class QuickUnion:
+class WeightedQuickUnion:
 
   nodes = []
+  size = []
 
   def __init__(self, totalElements):
     for i in range(totalElements):
       self.nodes.append(i)
+      self.size.append(1)
   
   def __repr__(self):
     return "QuickUnion{}".format(tuple(self.nodes))
 
-  # My implementation
-  # def findRoot(self, i):
-  #   for _ in self.nodes:
-  #     root = self.nodes[self.nodes[i]]
-  #     if self.nodes.index(root) == root: 
-  #       break
-  #   return root
-
-  # Courses implementation
   def findRoot(self, i):
     while i != self.nodes[i]:
       i = self.nodes[i]
@@ -25,7 +18,14 @@ class QuickUnion:
   
   def union(self, p, q):
     if self.connected(p, q): return
-    self.nodes[self.findRoot(q)] = self.findRoot(p)
+    
+    p, q = self.findRoot(p), self.findRoot(q)
+    if self.size[p] > self.size[q]:
+      self.nodes[q] = p
+      self.size[p] += self.size[q]
+    else:
+      self.nodes[p] = q
+      self.size[q] += self.size[p]
 
   def connected(self, p, q):
     return self.findRoot(p) == self.findRoot(q)
@@ -35,6 +35,6 @@ with open("tinyUF.txt", "r") as f:
   data = f.read().split("\n")
   links = [tuple(i.split(" ")) for i in data[1:-1]]
   
-qu = QuickUnion(int(data[0]))
+qu = WeightedQuickUnion(int(data[0]))
 for p, q in links:
   qu.union(int(p), int(q))
